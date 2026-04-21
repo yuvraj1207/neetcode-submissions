d80ba2847c7532c4d792fdcc316b82f758c0602e
yuvraj1207 <142510812+yuvraj1207@users.noreply.github.com>
2026-04-21 19:53:39 +0530

    Delete Data Structures & Algorithms/validate-parentheses/submission-3.py

--- Data Structures & Algorithms/validate-parentheses/submission-3.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        prev = None
-        
-        while prev != s:
-            prev = s
-            s = s.replace("()", "").replace("{}", "").replace("[]", "")
-        
-        return s == ""
